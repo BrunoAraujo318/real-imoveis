94657315bf1a92f2e9f0be8e6350862cd117cb2d
Allan Gonçalves da Cruz <allangcruz@gmail.com>
2017-08-29 19:42:32 -0300

    corrigindo model do usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,10 +2,28 @@
 
 namespace RealImoveis\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
     public function Telefone()
     {
     	return $this->hasMany('RealImoveis\Telefone','telefones');
--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => RealImoveis\User::class,
+            'model' => RealImoveis\Models\Usuario::class,
         ],
 
         // 'users' => [
